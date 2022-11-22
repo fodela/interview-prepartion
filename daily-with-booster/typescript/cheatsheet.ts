643b493fcd9ab59f0141f13b7880281b40aaac12
fodela <particleme30@gmail.com>
2022-11-22 19:21:36 +0000

    typescript: renamed author

--- daily-with-booster/typescript/cheatsheet.ts
+++ daily-with-booster/typescript/cheatsheet.ts
@@ -3,3 +3,11 @@
 const myName : string = "Hello World!"
 const myAge : number = 24
 const imHandsome : boolean = true
+
+// It is better to let typescript infer the type
+const myFavFood : {
+   name:string 
+   preference: number
+} = {name: "spag",
+    preference: 1    
+}
