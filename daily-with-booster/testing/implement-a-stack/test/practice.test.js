625857db2358da55371fcd24277730728f5b283c
fodela <particleme30@gmail.com>
2022-12-08 04:50:51 +0000

    testing: practice array test implementation

--- daily-with-booster/testing/implement-a-stack/test/practice.test.js
+++ daily-with-booster/testing/implement-a-stack/test/practice.test.js
@@ -1,7 +1,12 @@
-const { sum, array } = require("../src/sum");
+const { sum, array } = require("../src/practice");
 
 describe("Practice testing with jest", () => {
   it("properly adds two numbers", () => {
     expect(sum(1, 2)).toBe(3);
+  });
+
+  it("create a copy of array in memory", () => {
+    expect(array([1, 2, 3])).toEqual([1, 2, 3]);
+    expect(array([1, 2, 3])).not.toBe([1, 2, 3]);
   });
 });
